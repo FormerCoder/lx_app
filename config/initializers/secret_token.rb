9bfc6fb88272a46cb5b052fcae4ae11d77d95b9a
FormerCoder <atma_Shetty@hotmail.com>
2012-10-27 13:21:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LxApp::Application.config.secret_token = '008ddf7cc6021a625494a984b79ab5a7fb55db8e613ebe66645bb8b98ca20ce0de580c5956082da909eeade8c2564f8e0d012a130e8871a275c911aff10d7def'
